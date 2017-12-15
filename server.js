91dabcd969f0ae52dbdd25ea2dce227555cf3335
Mark Smith <smudger007@googlemail.com>
2017-12-15 17:29:22 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+// hbs is a 'Handlebars' wrapper module. Handlebars is a Templating Engine.
+
+var app = express();
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+// Register some Express Middleware.
+
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res,next) => {
+
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} - ${req.url} `;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log(`ERROR: ${err}`);
+        }
+    } );
+
+    next();
+});
+
+
+
+
+app.get('/', (req,res) => {
+    
+    res.render('home.hbs', {
+        pageTitle: 'Home Page...',  
+        welcomeMessage: 'Hello Smudger - this is the welcome page'
+    });
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page...'
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'FATAL: System has crashed', 
+        test: 'This is just a test message'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
