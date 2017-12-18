449a141dd7a8739ed58b96480343b94925c45710
Mark Smith <smudger007@googlemail.com>
2017-12-18 17:04:02 +0000

    Add Projects Page

--- server.js
+++ server.js
@@ -59,6 +59,12 @@ app.get('/about', (req,res) => {
     });
 });
 
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
+
 app.get('/bad', (req,res) => {
     res.send({
         errorMessage: 'FATAL: System has crashed', 
