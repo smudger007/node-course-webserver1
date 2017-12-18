f559bef632f1deedcd73cf9c8dea2164d6b89f96
Mark Smith <smudger007@googlemail.com>
2017-12-18 15:47:28 +0000

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 // hbs is a 'Handlebars' wrapper module. Handlebars is a Templating Engine.
 
 var app = express();
@@ -19,9 +21,9 @@ hbs.registerHelper('screamIt', (text) => {
 
 // Register some Express Middleware.
 
-app.use((req,res,next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req,res,next) => {
+//     res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
@@ -64,6 +66,6 @@ app.get('/bad', (req,res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
